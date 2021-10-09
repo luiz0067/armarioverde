a206082d158dcdbf174fb140d08f6337b3d7d2fd
Luiz Fernando Brogliatto Ferreira <luiz0067@gmail.com>
2021-10-09 14:48:29 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link rel="stylesheet" type="text/css" href="estilo.css" />
+<title>Armário Verde</title>
+</head>
+
+<body>
+<div id="barra-cima"></div>
+<div id="logo"></div>
+<div id="container-contato">
+	<div id="escrita"><img src="escrita.png" width="424" height="75" border="0" /></div>
+    <div id="form-mail">
+    	<form action="enviar.php" method="post" name="formulario">
+        	Nome:<input type="text" class="inputs" name="nome" /><br><br>
+            Fone:<input type="text" class="inputs" name="fone"  /><br><br>
+            Cidade:<input type="text" class="inputs" name="cidade"  /><br><br>
+            E-mail:<input type="text"  class="inputs" name="email" /><br><br>
+            <input type="submit" name="enviar" value="Enviar" class="bt" />
+            <input type="reset" name="limpar" value="Limpar" class="bt" />
+        </form>
+    </div>
+</div>
+</body>
+<div id="barra-baixo"><div id="mix"><a href="http://www.studiomidiamix.com.br"><img src="midiamix.png" width="67" height="20" border="0" /></a></div></div>
+</html>
